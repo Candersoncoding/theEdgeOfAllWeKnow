b9be47a0fd443fd32c74580619443c121632477c
Candersoncoding <soccercda88@aol.com>
2022-01-30 00:40:40 -0600

    fixed initial styling

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,45 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Router} from '@reach/router';
 import Main from './views/Main.jsx';
-import HeadingStyle from './components/HeadingStyle.jsx';
+
 
 function App() {
   // useEffect(()=> {
 
   // })
+
+  const styleHeader = {
+    height: "200px",
+    backgroundColor: "#242424",
+    color: "white",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingBottom: "20px",
+    margin: "0px"
+  }
+
+  const asideNavbar = {
+    width: "25%",
+    backgroundColor: "#56585d",
+    height: "100vh",
+    color: "white",
+    padding: "25px"
+  }
+
+  const bodyStyling = {
+    backgroundColor: "#a7a7a7",
+    height: "100vh",
+    width: "75%"
+  }
+
   return (
     <div className="App">
+
         <Router>
-          <Main path={"/"} />
+          <Main path={"/"} styleHeader={styleHeader} navBar={asideNavbar} bodyStyling={bodyStyling}/>
         </Router>
+
     </div>
   );
 }
--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import HeadingStyle from '../components/HeadingStyle';
 
-const Main = () => {
+
+const Main = (props) => {
 
     // useEffect(() => {
     //     axios.get('')
@@ -12,9 +12,15 @@ const Main = () => {
 
     return ( 
         <div>
-            <HeadingStyle>
-                <h1 className='display-1'>The Edge of All We Know</h1>
-            </HeadingStyle>
+            <h1 className='display-1' style={props.styleHeader}>The Edge of All We Know</h1>
+            <div className='d-flex'>
+                <nav style={props.navBar}>
+                    
+                </nav>
+                <div style={props.bodyStyling}>
+                    
+                </div>
+            </div> 
         </div>
     )
 }
