f0c4e06e598a5e7a60fcbfc3c92bb858951f8e83
Candersoncoding <soccercda88@aol.com>
2022-02-07 20:19:58 -0600

    minor styling SinglePlanet.jsx

--- client/src/components/SinglePlanet.jsx
+++ client/src/components/SinglePlanet.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, {useState, useEffect} from 'react';
-import Mercury from '../images/Mercury.jpg';
+import Mercury from '../images/mercuryColor.jpg';
 
 const SinglePlanet = (props) => {
 
@@ -11,17 +11,36 @@ const SinglePlanet = (props) => {
     //         .then(res => console.log(res.data))
     //         .catch(err => console.log(err))
     // }, [])
+
+    const planetJumbotron = {
+        backgroundColor: "#000000",
+        opacity: ".80",
+        borderRadius: "10px",
+        padding: "25px",
+        color: "#c2c2c2"
+    }
+
+    const imageStyle = {
+        minWidth: "50%",
+        maxWidth: "75%"
+    }
+
     return( 
-        <div>
-            <div className="jumbotron">
+        <div className='p-5'>
+            <div className='jumbotron' style= {planetJumbotron} >
                 <h1 className="display-3">{props.planet.englishName}</h1>
-                <p className="lead">Average Temperature: {props.planet.avgTemp} Kelvin</p>
-                <p className="lead">Number of orbiting Moons: {props.planet.moons == null ? '0' : props.planet.moons.length}</p>
-                <p className="lead">Radius of Planet: {props.planet.meanRadius} km.</p>
-                <p className="lead">Radius of Planet: {props.planet.gravity} m/s^2</p>
+                <img src={Mercury} alt="image of planet" style={imageStyle} />
                 <hr className="my-2"></hr>
-                <img src={Mercury} alt="image of planet" className='w-50'/>
-                
+                <div className='d-flex justify-content-around'>
+                    <div>
+                        <p className="lead"><strong>Average Temperature:</strong> {props.planet.avgTemp} Kelvin</p>
+                        <p className="lead"><strong>Number of orbiting Moons:</strong> {props.planet.moons == null ? '0' : props.planet.moons.length}</p>
+                    </div>
+                    <div>
+                        <p className="lead"><strong>Radius of Planet:</strong> {props.planet.meanRadius} km.</p>
+                        <p className="lead"><strong>Radius of Planet:</strong> {props.planet.gravity} m/s<sup>2</sup></p>
+                    </div>
+                </div>
             </div>
         </div>
     )
