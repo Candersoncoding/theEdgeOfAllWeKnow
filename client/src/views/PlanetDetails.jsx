4f1bb0ed49e5197d15ab74ccd1150956d0831ab8
Candersoncoding <soccercda88@aol.com>
2022-02-13 12:24:27 -0600

    added comments and navbar title

--- client/src/views/PlanetDetails.jsx
+++ client/src/views/PlanetDetails.jsx
@@ -1,5 +1,5 @@
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const PlanetDetails = (props) => {
 
@@ -13,6 +13,7 @@ const PlanetDetails = (props) => {
 				)
       .catch(err => console.log(err))
   }, [])
+  
 
   console.log(singlePlanetDetails);
 
@@ -28,30 +29,45 @@ const PlanetDetails = (props) => {
 			<div className='d-flex'>
 				<div style={props.bodyStyling}>
 					<div className="jumbotron text-light m-5 bg-dark p-3 rounded">
-					<h1 className="display-5">Let's Find Out More!</h1>
-					<p className="lead"></p>
-					<hr className="my-2"></hr>
-					<p className="lead">number of moons: {singlePlanetDetails.moons.length}</p>
-					<p className="lead">perihelion: {singlePlanetDetails.perihelion}</p>
-					<p className="lead">aphelion: {singlePlanetDetails.aphelion}</p>
-					<p className="lead">mass: {singlePlanetDetails.mass.massValue} <sup>{singlePlanetDetails.mass.massExponent}</sup></p>
-					<p className="lead">volume: {singlePlanetDetails.vol.volValue} <sup>{singlePlanetDetails.vol.volExponent}</sup></p>
-					<p className="lead">density: {singlePlanetDetails.density}</p>
-					<p className="lead">gravity: {singlePlanetDetails.gravity}</p>
-					<p className="lead">escape radius: {singlePlanetDetails.escape}</p>
-					<p className="lead">equator radius: {singlePlanetDetails.equaRadius}</p>
-					<p className="lead">sideral orbit: {singlePlanetDetails.sideralOrbit}</p>
-					<p className="lead">sideral rotation: {singlePlanetDetails.sideralRotation}</p>
-					<p className="lead">discovered by: {singlePlanetDetails.discoveredBy}</p>
-					<p className="lead">discovered on:{singlePlanetDetails.discoveryDate}</p>
-					<p className="lead">axial tilt: {singlePlanetDetails.axialTilt}</p>
-					<p className="lead">average temp: {singlePlanetDetails.avgTemp}</p>
-					
-					
+						<h1 className="display-5">Let's Find Out More!</h1>
+						<p className="lead"></p>
+						
+						
+						<hr className="my-2"></hr>
+						<div className='d-flex justify-content-evenly'>
+							<div>
+								<p className="lead">number of moons: {singlePlanetDetails.moons.length}</p>
+								<p className="lead">perihelion: {singlePlanetDetails.perihelion} km</p>
+								<p className="lead">aphelion: {singlePlanetDetails.aphelion} km</p>
+								<p className="lead">mass: {singlePlanetDetails.mass.massValue} <sup>{singlePlanetDetails.mass.massExponent}</sup> kg</p>
+								<p className="lead">volume: {singlePlanetDetails.vol.volValue} <sup>{singlePlanetDetails.vol.volExponent}</sup> km</p>
+								<p className="lead">density: {singlePlanetDetails.density} kg</p>
+								<p className="lead">gravity: {singlePlanetDetails.gravity} m/s<sup>2</sup></p>
+							</div>
+							<div>
+								<p className="lead">escape radius: {singlePlanetDetails.escape} km</p>
+								<p className="lead">equator radius: {singlePlanetDetails.equaRadius} km</p>
+								<p className="lead">sideral orbit: {singlePlanetDetails.sideralOrbit} Earth days</p>
+								<p className="lead">sideral rotation: {singlePlanetDetails.sideralRotation} hours</p>
+								<p className="lead">discovered by: {singlePlanetDetails.discoveredBy}</p>
+								<p className="lead">discovered on:{singlePlanetDetails.discoveryDate}</p>
+								<p className="lead">axial tilt: {singlePlanetDetails.axialTilt} degrees</p>
+								<p className="lead">average temp: {singlePlanetDetails.avgTemp} degrees Kelvin</p>
+							</div>
+						</div>
 					</div>
 				</div>
-				<nav style={props.navBar}>
-					<button className='btn btn-outline-light'>other stuff</button>
+				<nav style={props.navBar} className='d-flex flex-column'>
+					<h5>Moons of {singlePlanetDetails.englishName}:</h5>
+					{
+						singlePlanetDetails.moons.map((item, i) => {
+							return <button className="btn btn-outline-light m-1">{item.moon}</button>
+							// later make the button a link or make an onClick function that makes
+							// an api call for the rel property to get that specific moons data and
+							// display it
+							// probably will need a state for selecting the clicked moon 
+						})
+					}
 				</nav>
 			</div>
 			
