c79c6391a1d4ae7112a022bc6be8bb8b27e28748
Candersoncoding <soccercda88@aol.com>
2022-02-12 23:55:03 -0600

    added data fields for PlanetDetails.jsx

--- client/src/views/PlanetDetails.jsx
+++ client/src/views/PlanetDetails.jsx
@@ -27,27 +27,27 @@ const PlanetDetails = (props) => {
 			</header>
 			<div className='d-flex'>
 				<div style={props.bodyStyling}>
-					<div className="jumbotron text-light m-5 bg-dark p-3">
+					<div className="jumbotron text-light m-5 bg-dark p-3 rounded">
 					<h1 className="display-5">Let's Find Out More!</h1>
 					<p className="lead"></p>
 					<hr className="my-2"></hr>
-					<p className="lead"></p>
-					<table className="table table-hover text-light">
-						<thead className='thead-light'>
-							<tr>
-								<th>#</th>
-								<th></th>
-								<th>Last Name</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<th scope="row">1</th>
-								<td>Mark</td>
-								<td>Otto</td>
-							</tr>
-						</tbody>
-					</table>
+					<p className="lead">number of moons: {singlePlanetDetails.moons.length}</p>
+					<p className="lead">perihelion: {singlePlanetDetails.perihelion}</p>
+					<p className="lead">aphelion: {singlePlanetDetails.aphelion}</p>
+					<p className="lead">mass: {singlePlanetDetails.mass.massValue} <sup>{singlePlanetDetails.mass.massExponent}</sup></p>
+					<p className="lead">volume: {singlePlanetDetails.vol.volValue} <sup>{singlePlanetDetails.vol.volExponent}</sup></p>
+					<p className="lead">density: {singlePlanetDetails.density}</p>
+					<p className="lead">gravity: {singlePlanetDetails.gravity}</p>
+					<p className="lead">escape radius: {singlePlanetDetails.escape}</p>
+					<p className="lead">equator radius: {singlePlanetDetails.equaRadius}</p>
+					<p className="lead">sideral orbit: {singlePlanetDetails.sideralOrbit}</p>
+					<p className="lead">sideral rotation: {singlePlanetDetails.sideralRotation}</p>
+					<p className="lead">discovered by: {singlePlanetDetails.discoveredBy}</p>
+					<p className="lead">discovered on:{singlePlanetDetails.discoveryDate}</p>
+					<p className="lead">axial tilt: {singlePlanetDetails.axialTilt}</p>
+					<p className="lead">average temp: {singlePlanetDetails.avgTemp}</p>
+					
+					
 					</div>
 				</div>
 				<nav style={props.navBar}>
